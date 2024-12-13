8c74d2dccae477e51e396135ecbf39b15560e57b
lucasoneves <casluhc@gmail.com>
2024-12-13 18:49:01 -0300

    Adding Go modules

---
+++ contas/contaCorrente.go
@@ -0,0 +1,39 @@
+package contas
+
+type ContaCorrente struct {
+	Titular       string
+	NumeroAgencia int
+	NumeroConta   int
+	Saldo         float64
+}
+
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.Saldo
+
+	if podeSacar {
+		c.Saldo -= valorDoSaque
+		return "Saque efetuado!"
+	}
+
+	return "Saque recusado - Saldo insuficiente"
+}
+
+func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
+
+	if valorDoDeposito > 0 {
+		c.Saldo += valorDoDeposito
+		return "Depósito efetuado! Saldo:", c.Saldo
+	}
+
+	return "Valor do depósito menor que zero", c.Saldo
+}
+
+func (c *ContaCorrente) Transferencia(valorDaTransf float64, contaDestino *ContaCorrente) bool {
+	if valorDaTransf < c.Saldo && valorDaTransf > 0 {
+		c.Saldo -= valorDaTransf
+		contaDestino.Depositar(valorDaTransf)
+		return true
+	} else {
+		return false
+	}
+}
--- main.go
+++ main.go
@@ -1,50 +1,15 @@
 package main
 
-import "fmt"
-
-type ContaCorrente struct {
-	titular       string
-	numeroAgencia int
-	numeroConta   int
-	saldo         float64
-}
-
-func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
-	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
-
-	if podeSacar {
-		c.saldo -= valorDoSaque
-		return "Saque efetuado!"
-	}
-
-	return "Saque recusado - Saldo insuficiente"
-}
-
-func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
-
-	if valorDoDeposito > 0 {
-		c.saldo += valorDoDeposito
-		return "Depósito efetuado! Saldo:", c.saldo
-	}
-
-	return "Valor do depósito menor que zero", c.saldo
-}
-
-func (c *ContaCorrente) Transferencia(valorDaTransf float64, contaDestino *ContaCorrente) bool {
-	if valorDaTransf < c.saldo && valorDaTransf > 0 {
-		c.saldo -= valorDaTransf
-		contaDestino.Depositar(valorDaTransf)
-		return true
-	} else {
-		return false
-	}
-}
+import (
+	"banco/contas"
+	"fmt"
+)
 
 func main() {
 
-	contaDoLucas := ContaCorrente{titular: "Lucas", saldo: 300}
+	contaDoLucas := contas.ContaCorrente{Titular: "Lucas", Saldo: 300}
 
-	contaDoGustavo := ContaCorrente{titular: "Gustavo", saldo: 1000}
+	contaDoGustavo := contas.ContaCorrente{Titular: "Gustavo", Saldo: 1000}
 	fmt.Println(contaDoGustavo)
 	fmt.Println(contaDoLucas)
 
