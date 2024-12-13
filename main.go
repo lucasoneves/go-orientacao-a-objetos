ab7dbda2e7fe41144996fbba4e9a58fa5bd947e7
lucasoneves <casluhc@gmail.com>
2024-12-13 16:50:37 -0300

    Múltiplos retornos, transferência entre contas

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 type ContaCorrente struct {
 	titular       string
@@ -11,12 +9,50 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque efetuado!"
+	}
+
+	return "Saque recusado - Saldo insuficiente"
+}
+
+func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
+
+	if valorDoDeposito > 0 {
+		c.saldo += valorDoDeposito
+		return "Depósito efetuado! Saldo:", c.saldo
+	}
+
+	return "Valor do depósito menor que zero", c.saldo
+}
+
+func (c *ContaCorrente) Transferencia(valorDaTransf float64, contaDestino *ContaCorrente) bool {
+	if valorDaTransf < c.saldo && valorDaTransf > 0 {
+		c.saldo -= valorDaTransf
+		contaDestino.Depositar(valorDaTransf)
+		return true
+	} else {
+		return false
+	}
+}
+
 func main() {
 
 	contaDoLucas := ContaCorrente{titular: "Lucas", saldo: 300}
-	contaDaCamila := ContaCorrente{"Camila", 222, 34434, 400}
 
+	contaDoGustavo := ContaCorrente{titular: "Gustavo", saldo: 1000}
+	fmt.Println(contaDoGustavo)
 	fmt.Println(contaDoLucas)
-	fmt.Println(contaDaCamila)
+
+	status := contaDoLucas.Transferencia(150, &contaDoGustavo)
+
+	fmt.Println(status)
+
+	fmt.Println(contaDoGustavo)
+	fmt.Println(contaDoLucas)
 
 }
