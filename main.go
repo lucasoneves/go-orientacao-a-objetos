4786a10d074e336199040d525786d446d4f6e45e
lucasoneves <casluhc@gmail.com>
2024-12-18 19:05:29 -0300

    Final

--- main.go
+++ main.go
@@ -16,15 +16,26 @@ type verificarConta interface {
 
 func main() {
 	pessoa1 := clientes.Titular{Nome: "Lucas", CPF: "22334455", Profissao: "Software Engineer"}
+
+	pessoa2 := clientes.Titular{Nome: "Camila", CPF: "23345591", Profissao: "Analista de RH"}
 	contaPoupancaDoLucas := contas.ContaPoupanca{Titular: pessoa1, NumeroAgencia: 2234, NumeroConta: 2234}
 
+	contaDaCamila := contas.ContaCorrente{Titular: pessoa2, NumeroAgencia: 2340, NumeroConta: 12}
+
 	contaPoupancaDoLucas.Depositar(200)
+
+	contaDaCamila.Depositar(12000)
 
 	// contaPoupancaDoLucas.ObterSaldo()
 
 	fmt.Println(contaPoupancaDoLucas)
 
+	fmt.Println(contaDaCamila.ObterSaldo())
+
 	PagarBoleto(&contaPoupancaDoLucas, 100)
 
+	PagarBoleto(&contaDaCamila, 300)
+
 	fmt.Println(contaPoupancaDoLucas)
+	fmt.Println(contaDaCamila.ObterSaldo())
 }
