de07c9259c2e490f16c52b7d495beadcc644265b
lucasoneves <casluhc@gmail.com>
2024-12-18 18:08:25 -0300

    Interface

--- main.go
+++ main.go
@@ -6,16 +6,25 @@ import (
 	"fmt"
 )
 
+func PagarBoleto(conta verificarConta, valorDoBoleto float64) {
+	conta.Sacar(valorDoBoleto)
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string
+}
+
 func main() {
 	pessoa1 := clientes.Titular{Nome: "Lucas", CPF: "22334455", Profissao: "Software Engineer"}
 	contaPoupancaDoLucas := contas.ContaPoupanca{Titular: pessoa1, NumeroAgencia: 2234, NumeroConta: 2234}
 
 	contaPoupancaDoLucas.Depositar(200)
 
-	contaPoupancaDoLucas.Sacar(50)
-
-	contaPoupancaDoLucas.ObterSaldo()
+	// contaPoupancaDoLucas.ObterSaldo()
 
 	fmt.Println(contaPoupancaDoLucas)
-	fmt.Println(contaPoupancaDoLucas.ObterSaldo())
+
+	PagarBoleto(&contaPoupancaDoLucas, 100)
+
+	fmt.Println(contaPoupancaDoLucas)
 }
